b092ad1afa6dedad86524b70098e0c8053eb34f1
Ryan Wong <ryan.wong022@gmail.com>
2015-10-11 22:49:59 +0800

    added readme file

--- requirements.py
+++ requirements.py
@@ -1,4 +1,9 @@
 Django==1.8.5
+django-allauth==0.23.0
 django-rest-auth==0.5.0
 djangorestframework==3.2.4
+oauthlib==1.0.3
+python-openid==2.2.5
+requests==2.8.0
+requests-oauthlib==0.5.0
 six==1.10.0
